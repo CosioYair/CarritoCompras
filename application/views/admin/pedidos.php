bcbf1116a757374cf416c14f61959295875651b1
alexis buenrostro <alexis.buenrostro@e-omnisys.com>
2018-02-24 20:27:59 -0600

    vista pedidos

---
+++ application/views/admin/pedidos.php
@@ -0,0 +1,51 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        <?php echo "Pedidos"; ?>
+        <small>Panel de control</small>
+      </h1>
+    </section>
+
+	<!-- Main content -->
+	<section class="content">
+		<!-- Info boxes -->
+		<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+				<table id="table_pedidos" class="table table-striped table-dark table_pedidos">
+				  <thead>
+				    <tr>
+				      <th scope="col">#</th>
+				      <th scope="col">First</th>
+				      <th scope="col">Last</th>
+				      <th scope="col">Handle</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <th scope="row">1</th>
+				      <td>Mark</td>
+				      <td>Otto</td>
+				      <td>@mdo</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Jacob</td>
+				      <td>Thornton</td>
+				      <td>@fat</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">3</th>
+				      <td>Larry</td>
+				      <td>the Bird</td>
+				      <td>@twitter</td>
+				    </tr>
+				  </tbody>
+				</table>
+			</div>
+		</div>
+	</section>  
+</div>
+<!-- /.content-wrapper -->
+
