10ccb898800fa1ab1b9a8265866fd3c05b0b1b4e
alexis buenrostro <alexis.buenrostro@e-omnisys.com>
2018-02-24 18:04:13 -0600

    removiendo index.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'cart';
 $route['home'] = 'cart/home_view';
+$route['test'] = 'Cart_admin/test';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
