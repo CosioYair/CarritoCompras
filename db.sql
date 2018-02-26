a458e8aac597f661d6cbc5891f8c22a6176052d1
Yair <ycosio1310@gmail.com>
2018-02-26 17:01:06 -0600

    Bd line

--- db.sql
+++ db.sql
@@ -73,3 +73,4 @@ telefono varchar(20) not null,
 creacion timestamp default now()
 );
 ALTER TABLE pedido2cliente ADD precio_elegido_venta varchar(80) not null;
+ALTER TABLE pedido ADD descripcion text;
