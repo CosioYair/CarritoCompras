bb97423bbc25398e06a885d2f682cfd4c1738021
Yair <ycosio1310@gmail.com>
2018-02-26 18:51:52 -0600

    Login and axios function implementing

--- assets/js/vueModules/login.js
+++ assets/js/vueModules/login.js
@@ -1,10 +1,22 @@
+import axios from 'axios';
 var login = {
   prop: {
-    errorMessage: "Usuario o contrasena invalida"
+    errorMessage: "Usuario o contrasena invalida",
+    email: '',
+    password: ''
   },
   method: {
-
+    loginUser: loginUser
   }
+}
+
+function loginUser(){
+  axios.post('login/loginUser', {
+    email: login.prop.email,
+    password: login.prop.password
+  }).then(response => {
+    console.log(response.date);
+  });
 }
 
 module.exports = login;
